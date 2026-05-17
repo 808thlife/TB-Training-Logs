9fc81f4001e2e14f7710497f8c4170a772f6100f
808thlife <khqyop@gmail.com>
2026-05-17 14:26:05 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/tb_training_logs/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tb_training_logs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/inital_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InitalScreen extends StatelessWidget {
+  const InitalScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
