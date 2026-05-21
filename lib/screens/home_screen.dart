79937a4a62bfa3bd2edbf6ff937f1998523bc8b3
808thlife <khqyop@gmail.com>
2026-05-21 11:54:06 +0500

    edited home scren

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:tb_training_logs/enums/plan_status.dart';
 import 'package:tb_training_logs/models/training_plan.dart';
-import 'package:tb_training_logs/widgets/no_plan_view.dart';
 import 'package:tb_training_logs/widgets/plans_list_home_page.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key, required this.plans});
 
   final List<TrainingPlan> plans;
+  List<TrainingPlan> byStatus(List<TrainingPlan> plans, PlanStatus status) {
+    return plans.where((p) => p.status == status).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (plans.isEmpty) {
-      return const NoPlanView();
-    }
+    final active = byStatus(plans, PlanStatus.active);
 
-    return PlansListHomePage(plans: plans);
+    final completed = byStatus(plans, PlanStatus.completed);
+
+    final archived = byStatus(plans, PlanStatus.archived);
+
+    return DefaultTabController(
+      length: 3,
+
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("My Plans"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Handle add new plan action
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Active"),
+
+              Tab(text: "Completed"),
+
+              Tab(text: "Archived"),
+            ],
+          ),
+        ),
+
+        body: TabBarView(
+          children: [
+            PlansList(plans: active),
+
+            PlansList(plans: completed),
+
+            PlansList(plans: archived),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/plans_list_home_page.dart
+++ lib/widgets/plans_list_home_page.dart
@@ -3,72 +3,67 @@ import 'package:intl/intl.dart';
 import 'package:tb_training_logs/models/training_plan.dart';
 import 'package:tb_training_logs/screens/active_plan_view_screen.dart';
 
-class PlansListHomePage extends StatelessWidget {
-  const PlansListHomePage({super.key, required this.plans});
+class PlansList extends StatelessWidget {
+  const PlansList({super.key, required this.plans});
 
   final List<TrainingPlan> plans;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("My Plans")),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(12),
-        itemCount: plans.length,
-        itemBuilder: (context, index) {
-          final plan = plans[index];
+    return ListView.builder(
+      padding: const EdgeInsets.all(12),
+      itemCount: plans.length,
+      itemBuilder: (context, index) {
+        final plan = plans[index];
 
-          return GestureDetector(
-            onLongPress: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: const Text("Delete Plan"),
-                  content: const Text(
-                    "Are you sure you want to delete this plan?",
+        return GestureDetector(
+          onLongPress: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text("Delete Plan"),
+                content: const Text(
+                  "Are you sure you want to delete this plan?",
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text("Cancel"),
                   ),
-                  actions: [
-                    TextButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text("Cancel"),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        // Handle delete action
-                      },
-                      child: Text(
-                        "Delete",
-                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                          color: Theme.of(context).colorScheme.error,
-                        ),
+                  TextButton(
+                    onPressed: () {
+                      // Handle delete action
+                    },
+                    child: Text(
+                      "Delete",
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.error,
                       ),
                     ),
-                  ],
-                ),
-              );
-            },
-            child: Card(
-              child: ListTile(
-                title: Text(plan.name),
-                subtitle: Text(
-                  "${DateFormat('MMM d').format(plan.startDate)} "
-                  "- "
-                  "${DateFormat('MMM d').format(plan.endDate)}",
-                ),
-                trailing: const Icon(Icons.arrow_forward_ios),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => ActivePlanView(plan: plan),
-                    ),
-                  );
-                },
+                  ),
+                ],
               ),
+            );
+          },
+          child: Card(
+            child: ListTile(
+              title: Text(plan.name),
+              subtitle: Text(
+                "${DateFormat('MMM d').format(plan.startDate)} "
+                "- "
+                "${DateFormat('MMM d').format(plan.endDate)}",
+              ),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => ActivePlanView(plan: plan)),
+                );
+              },
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
